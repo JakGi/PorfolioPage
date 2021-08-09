b17d7997ef5d6c131cf941f8f39b9d949aa1d750
JakubGala <snuq321@gmail.com>
2021-08-09 23:05:06 +0200

    added main page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="porfoliopicture" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Jakub Gala</h1>
+                    <h3>Freelance <span></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="down" />
+                </a>
+            </div>
         </div>
     )
 }
