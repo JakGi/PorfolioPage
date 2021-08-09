5dc7c7b97aa9b10153a2e0cb823af1c1e7992bf1
JakubGala <snuq321@gmail.com>
2021-08-09 15:36:27 +0200

    added navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,14 @@
-import "./topbar.scss"
+import "./topbar.scss";
 
 export default function Topbar() {
-    return (
-        <div className="topbar">
-            
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">Front-end Developer</a>
         </div>
-    )
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 }
