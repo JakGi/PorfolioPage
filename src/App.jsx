7e64283d34f517ba0f08f96c4b926c1cf12135b5
JakubGala <snuq321@gmail.com>
2021-08-09 11:41:59 +0200

    added react components

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro"
+import Porfolio from "./components/portfolio/Portfolio"
+import Works from "./components/works/Works"
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+
+function App() {
+  return <div className="app">
+    <Topbar />
+    <div className="sections">
+      <Intro />
+      <Porfolio />
+      <Works />
+      <Testimonials />
+      <Contact />
+    </div>
+  </div>;
+}
+
+export default App;
