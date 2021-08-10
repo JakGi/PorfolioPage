addbf7ebb03b77849a17de8f17afe865b34a1fe2
JakubGala <snuq321@gmail.com>
2021-08-10 09:12:32 +0200

    added animated text in main section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,23 +1,37 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
-    return (
-        <div className="intro" id="intro">
-            <div className="left">
-                <div className="imgContainer">
-                    <img src="assets/man.png" alt="porfoliopicture" />
-                </div>
-            </div>
-            <div className="right">
-                <div className="wrapper">
-                    <h2>Hi There, I'm</h2>
-                    <h1>Jakub Gala</h1>
-                    <h3>Freelance <span></span></h3>
-                </div>
-                <a href="#portfolio">
-                    <img src="assets/down.png" alt="down" />
-                </a>
-            </div>
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 2000,
+      strings: ["Web Developer!", "Front-end Developer!"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="porfoliopicture" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Jakub Gala</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="down" />
+        </a>
+      </div>
+    </div>
+  );
 }
