99aa6f925da66f2b161ac403068b69a85c7ec5bd
JakubGala <snuq321@gmail.com>
2021-08-10 11:02:36 +0200

    added testimonials

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,21 @@
 import "./portfolio.scss"
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Portfolio() {
+    const textRef = useRef();
+
+    useEffect(() => {
+      init(textRef.current, {
+        showCursor: true,
+        backDelay: 1000,
+        strings: ["In Progess!"],
+      });
+    }, []);
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1><span ref={textRef}></span></h1>
+           
         </div>
     )
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,52 @@
-import "./testimonials.scss"
+import "./testimonials.scss";
 
 export default function Testimonials() {
-    return (
-        <div className="testimonials" id="testimonials">
-            
-        </div>
-    )
+  const data = [
+    {
+      id: 1,
+      name: "InfoShare Academy",
+      img: "assets/infoshare.png",
+      desc: "Szkolenie obejmowało 324 godzin warszatowych w trypie dziennym.Dostarczyło ono wiedzy i praktycznych umiejetności z zakresu : HTML, CSS , SCSS, JS, React, Redux, Firebase, TypeSctipt, Gti, HTTP, Scrum, NodeJS, Webpack, ESlint, Babel, Prettier, Husky.",
+      link: <a href="http://infoshareacademy.com" alt="infoshare" target="_blank" rel="noreferrer">InfoShareAcademy</a>,
+      featured: false,
+    },
+    {
+        id: 2,
+        name: "InfoShare Academy",
+        img: "assets/infoshare.png",
+        desc: "Szkolenie obejmowało 324 godzin warszatowych w trypie dziennym.Dostarczyło ono wiedzy i praktycznych umiejetności z zakresu : HTML, CSS , SCSS, JS, React, Redux, Firebase, TypeSctipt, Gti, HTTP, Scrum, NodeJS, Webpack, ESlint, Babel, Prettier, Husky.",
+        link: <a href="http://infoshareacademy.com" alt="infoshare" target="_blank" rel="noreferrer">InfoShareAcademy</a>,
+        featured: true,
+      },
+      {
+        id: 3,
+        name: "InfoShare Academy",
+        img: "assets/infoshare.png",
+        desc: "Szkolenie obejmowało 324 godzin warszatowych w trypie dziennym.Dostarczyło ono wiedzy i praktycznych umiejetności z zakresu : HTML, CSS , SCSS, JS, React, Redux, Firebase, TypeSctipt, Gti, HTTP, Scrum, NodeJS, Webpack, ESlint, Babel, Prettier, Husky.",
+        link: <a href="http://infoshareacademy.com" alt="infoshare" target="_blank" rel="noreferrer">InfoShareAcademy</a>,
+        featured: false,
+      }
+  ];
+
+  return (
+    <div className="testimonials" id="testimonials">
+      <h1>Testimonials</h1>
+      <div className="container">
+        {data.map((d) => (
+          <div className={d.featured ? "card featured" : "card"}>
+            <div className="top">
+              <img className="user" src={d.img} alt=""/>
+            </div>
+            <div className="center">{d.desc}</div>
+            <div className="bottom">
+              <h3>{d.name}</h3>
+              <h4>
+                {d.link}
+              </h4>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
