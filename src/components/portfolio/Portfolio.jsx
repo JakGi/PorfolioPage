b14ad8d932c314ad020cdfc740dc789eb0e99d89
JakubGala <snuq321@gmail.com>
2021-08-12 13:39:30 +0200

    added porfolio projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
+import { webAppPortfolio } from "../../data";
+import { inProgressPortfolio } from "../../data";
+import { useEffect } from "react";
 
 export default function Portfolio() {
   const [selected, setSelected] = useState("webapp");
+  const [data, setData] = useState([]);
 
   const list = [
     {
@@ -15,6 +19,20 @@ export default function Portfolio() {
       title: "In progess",
     },
   ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "webapp":
+        setData(webAppPortfolio);
+        break;
+      case "inprogres":
+        setData(inProgressPortfolio);
+        break;
+      default:
+        setData(webAppPortfolio);
+    }
+  }, [selected]);
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Porfolio.</h1>
@@ -25,14 +43,17 @@ export default function Portfolio() {
             active={selected === item.id}
             setSelected={setSelected}
             id={item.id}
+            key={item.id} 
           />
         ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img src="/PorfolioPage/assets/sunsiadek.png" alt="comunityapp" />
-          <h3>Community App</h3>
-        </div>
+        {data.map((d) => (
+          <div className="item" key={d.id} >
+            <img src={d.img} alt="" />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,15 @@
+export const webAppPortfolio = [
+  {
+    id: 1,
+    title: "Social App",
+    img: "/PorfolioPage/assets/sunsiadek.png",
+  },
+];
+export const inProgressPortfolio = [
+  {
+    id: 1,
+    title: "8-weeks weightlifting program",
+    img: "/PorfolioPage/assets/8weeks.png",
+
+  },
+];
